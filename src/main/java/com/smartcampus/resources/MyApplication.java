8e0ae109db84287567e000bed9b6e158d666602a
TsinatG <gezahegnetsinat7@gmail.com>
2026-04-07 18:55:41 +0100

    Initial commit - SmartCampus REST API project setup

---
+++ src/main/java/com/smartcampus/resources/MyApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.smartcampus.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("rest")
+public class MyApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        Set<Class<?>> resources = new HashSet<>();
+
+        resources.add(HelloWorldResource.class);
+
+        return resources;
+    }
+}
---
+++ src/main/java/com/smartcampus/resources/MyApplicationConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.resources;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("rest")
+public class MyApplicationConfig extends ResourceConfig {
+
+    public MyApplicationConfig() {
+        register(HelloWorldResource.class);
+    }
+
+}
