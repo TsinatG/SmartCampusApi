eebcd58d3061b9a5bdca02b47ed6597f8df8e932
TsinatG <gezahegnetsinat7@gmail.com>
2026-04-16 12:21:16 +0100

    created the reading model

--- src/main/java/com/smartcampus/model/SensorReading.java
+++ src/main/java/com/smartcampus/model/SensorReading.java
@@ -4,10 +4,26 @@
  */
 package com.smartcampus.model;
 
-/**
- *
- * @author Abnet
- */
+import java.util.Date;
+
 public class SensorReading {
-    
+    private String id;
+    private double value;
+    private Date timestamp;
+
+    public SensorReading() {}
+
+    public SensorReading(String id, double value) {
+        this.id = id;
+        this.value = value;
+        this.timestamp = new Date(); // Sets to "now"
+    }
+
+    // Getters and Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public double getValue() { return value; }
+    public void setValue(double value) { this.value = value; }
+    public Date getTimestamp() { return timestamp; }
+    public void setTimestamp(Date timestamp) { this.timestamp = timestamp; }
 }
