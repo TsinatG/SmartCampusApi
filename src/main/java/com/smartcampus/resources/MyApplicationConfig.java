db8f4c6eeee0904abc786823d3d152a726a2fa10
TsinatG <gezahegnetsinat7@gmail.com>
2026-04-20 00:58:12 +0100

    Implemented centralized Exception Handling and resolve JAX-RS configuration conflicts

--- src/main/java/com/smartcampus/resources/MyApplicationConfig.java
+++ src/main/java/com/smartcampus/resources/MyApplicationConfig.java
@@ -1,18 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.smartcampus.resources;
+
 
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
+import com.smartcampus.mappers.RoomNotEmptyMapper; // 
+import com.smartcampus.mappers.LinkedResourceNotFoundMapper;
+import com.smartcampus.mappers.SensorUnavailableMapper;
+import com.smartcampus.mappers.GlobalExceptionMapper;
 
 @ApplicationPath("api/v1")
 public class MyApplicationConfig extends ResourceConfig {
 
     public MyApplicationConfig() {
-        register(SensorRoomResource.class);
+        
+        // 1. Manually register the Resources
+        register(RoomResource.class); 
         register(SensorResource.class);
+        
+        // 2. Manually register the Mapper (The most important part)
+        register(RoomNotEmptyMapper.class);  //409
+        register(LinkedResourceNotFoundMapper.class); //422
+        register(SensorUnavailableMapper.class); //403
+        register(GlobalExceptionMapper.class); //500
+        // 3. Force Jackson for JSON
+        register(org.glassfish.jersey.jackson.JacksonFeature.class);
+        
+        System.out.println("DEBUG: MyApplicationConfig has loaded!");
+        
+ 
+       
+        
+        
+        register(org.glassfish.jersey.jackson.JacksonFeature.class);
     }
-
 }
+        
+    
